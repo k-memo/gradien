5fbe02d0913c0b0932fad10ff6a4c8e4b8f0036b
Memo K <fhs49041@fh-salzburg.ac.at>
2024-04-23 15:50:13 +0200

    escape apos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Logo from "../../components/logo";
 import { FiPhone } from "react-icons/fi";
 import { FaRegEnvelope } from "react-icons/fa";
 import { VscLocation } from "react-icons/vsc";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { SlLocationPin } from "react-icons/sl";
 
 import "./style/application.scss";
@@ -14,12 +14,12 @@ export default function Home() {
     <main className="landing-page">
       <section className="landing-page-greeting">
         <div className="gradient-bg">
-            <div className="gradient-container">
-              <div className="g1"></div>
-              <div className="g2"></div>
-              <div className="g3"></div>
-              <div className="g4"></div>
-            </div>
+          <div className="gradient-container">
+            <div className="g1"></div>
+            <div className="g2"></div>
+            <div className="g3"></div>
+            <div className="g4"></div>
+          </div>
         </div>
         <div className="landing-text">
           <div className="heading">
@@ -30,20 +30,21 @@ export default function Home() {
             </h1>
             <p className="sub">FIND YOUR COLOR PALETTE</p>
           </div>
-          
+
           <button className="btn-special">
-          <div className="circle-icon">
-            <div className="circle">
+            <div className="circle-icon">
+              <div className="circle"></div>
+              <FontAwesomeIcon icon={faArrowRight} className="arrow-right" />
             </div>
-            <FontAwesomeIcon icon={faArrowRight} className="arrow-right" />
-          </div>
             Get Started
           </button>
         </div>
-       
       </section>
       <section id="about">
-        <h2>WHY YOU NEED <br/>COLOR ANALYSIS</h2>
+        <h2>
+          WHY YOU NEED <br />
+          COLOR ANALYSIS
+        </h2>
         <div className="bullet-points">
           <div className="points">
             <div>
@@ -75,30 +76,52 @@ export default function Home() {
       <section id="contact">
         <h2>CONTACT US</h2>
         <p className="contact-text">
-          Have a question or just want to say hello? We'd love to hear from you!
-          Feel free to reach out to us using the contact form, or connect with us via email or phone.
+          Have a question or just want to say hello? We&apos;d love to hear from
+          you! Feel free to reach out to us using the contact form, or connect
+          with us via email or phone.
         </p>
         <div className="contact-point">
-        <FiPhone />
-          <p><a href="tel:0000 00000000">0000 00000000</a></p>
+          <FiPhone />
+          <p>
+            <a href="tel:0000 00000000">0000 00000000</a>
+          </p>
         </div>
         <div className="contact-point">
-        <FaRegEnvelope />
-          <p><a href="mailto:0000000@0000.com">0000000@0000.com</a></p>
+          <FaRegEnvelope />
+          <p>
+            <a href="mailto:0000000@0000.com">0000000@0000.com</a>
+          </p>
         </div>
-       <div className="contact-point">
-       <SlLocationPin />
-        <p>000000000000000000</p>
-       </div>
-        
+        <div className="contact-point">
+          <SlLocationPin />
+          <p>000000000000000000</p>
+        </div>
 
         <div className="contact-form">
           <form action="/submit" method="post">
-            <label htmlFor="email">Email:</label><br />
-            <input type="email" id="email" name="email" placeholder="email@mail.com" required /><br /><br />
-            <label htmlFor="message">Message:</label><br />
-          <textarea id="message" name="message" rows={4} cols={50} required></textarea><br /><br />
-            <input type="submit" value="Submit" className="btn-main"/>
+            <label htmlFor="email">Email:</label>
+            <br />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="email@mail.com"
+              required
+            />
+            <br />
+            <br />
+            <label htmlFor="message">Message:</label>
+            <br />
+            <textarea
+              id="message"
+              name="message"
+              rows={4}
+              cols={50}
+              required
+            ></textarea>
+            <br />
+            <br />
+            <input type="submit" value="Submit" className="btn-main" />
           </form>
         </div>
       </section>
