cf38ad2f24661e39ddaa8c0c295dd12b0923d923
Helin Güyen <hgueyen.mmt-b2022@fh-salzburg.ac.at>
2024-04-23 16:15:37 +0200

    nav form logic

--- components/nav-form.tsx
+++ components/nav-form.tsx
@@ -1,34 +1,34 @@
-import { FaCheck } from "react-icons/fa6";
-import { useState } from "react";
+import { FaCheck } from "react-icons/fa";
+import { useState, useEffect } from "react";
 
-export default function NavForm() {
+export default function NavForm({ formStep }) {
     const [activeState, setActiveState] = useState(0); // Track the active state
+    useEffect(() => {
+        setActiveState(formStep);
+    }, [formStep]);
 
     const handleStateClick = (index) => {
         setActiveState(index);
     };
 
     // Calculate the width of .h-active based on the active state
-    const activeWidth = `${(activeState + 1) * 50}%`;
+    const activeWidth = `${(formStep + 1) * 50}%`;
 
-    return(
-        <>
-           <div className="nav-form">
+    return (
+        <div className="nav-form">
             <div className="h-active" style={{ width: activeWidth }}></div>
-                <div className="hr"></div>
-                <div className="states">
-                    {[...Array(3)].map((_, index) => (
-                        <div
-                            key={index}
-                            className={`state-circle ${activeState === index ? 'active' : ''}`}
-                            onClick={() => handleStateClick(index)}
-                        >
-                            {activeState >= index && <FaCheck />}
-                        </div>
-                    ))}
-                </div>
+            <div className="hr"></div>
+            <div className="states">
+                {[...Array(3)].map((_, index) => (
+                    <div
+                        key={index}
+                        className={`state-circle ${activeState >= index ? 'active' : ''}`}
+                        onClick={() => handleStateClick(index)}
+                    >
+                        {activeState >= index && <FaCheck />}
+                    </div>
+                ))}
             </div>
-        </>
-        
-    )
+        </div>
+    );
 }
