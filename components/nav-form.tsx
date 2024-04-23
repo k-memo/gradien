1b22f0bb97e68664bc0501a71d729659991d7451
Helin Güyen <hgueyen.mmt-b2022@fh-salzburg.ac.at>
2024-04-23 15:19:39 +0200

    gradien nav

--- components/nav-form.tsx
+++ components/nav-form.tsx
@@ -4,13 +4,17 @@ import { useState } from "react";
 export default function NavForm() {
     const [activeState, setActiveState] = useState(0); // Track the active state
 
-    const handleStateClick = (index: number) => {
+    const handleStateClick = (index) => {
         setActiveState(index);
     };
+
+    // Calculate the width of .h-active based on the active state
+    const activeWidth = `${(activeState + 1) * 50}%`;
+
     return(
         <>
            <div className="nav-form">
-            <div className="h-active"></div>
+            <div className="h-active" style={{ width: activeWidth }}></div>
                 <div className="hr"></div>
                 <div className="states">
                     {[...Array(3)].map((_, index) => (
