bb3d296d4c8c847e09165cdd066340f15fefb5b4
Memo K <fhs49041@fh-salzburg.ac.at>
2024-04-24 16:40:04 +0200

    rebase develop

--- src/app/generate/page.tsx
+++ src/app/generate/page.tsx
@@ -12,6 +12,9 @@ import NavForm from "../../../components/nav-form";
 import ShowMore from "../../../components/showmore";
 import { IPalette } from "../../../models/colorpalette.interface";
 import Logo from "../../../components/logo";
+import NavForm from "../../../components/nav-form";
+import ShowMore from "../../../components/showmore";
+import { IPalette } from "../../../models/colorpalette.interface";
 
 export default function Home() {
   const [formStep, setFormStep] = useState(0);
