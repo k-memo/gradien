c31e92cd2dffc39e6e18225f94b379fcd184f37e
Memo K <fhs49041@fh-salzburg.ac.at>
2024-04-25 16:56:59 +0200

    prestring and interface adjusted

--- models/colorpalette.interface.ts
+++ models/colorpalette.interface.ts
@@ -5,7 +5,11 @@ export interface IPalette {
     skin: string;
   };
 
+  userColorDisposition: string; // Text for the user, telling them what kind of color type they have etc
+
   colors: IPalletteColor[];
+
+  paletteInfo: string; // Explaining the general color palette, and why they were chosen for the user and the color psychology behind it
 
   jewelery: {
     color: 'gold'; // only gold or silver
--- models/prestring.const.ts
+++ models/prestring.const.ts
@@ -8,18 +8,22 @@ Very Important !!!!!!!!!!!!!!!!
 Your the json response should exactly be in in these template:
 
 export interface IPalette {
-initialColor: {
-  eyes: string; // eye color from request
-  hair: string; // hair color from request
-  skin: string; // skin color from request
-};
+    initialColor: {
+      eyes: string; // eye color from request
+      hair: string; // hair color from request
+      skin: string; // skin color from request
+    };
+    
+    userColorDisposition: string; // Text for the user, telling them what kind of color type they have etc
 
-colors: IPalletteColor[]; 
+    colors: IPalletteColor[]; 
 
-jewelery: {
-  color: string ; // only gold or silver
-  message: // a message why this color works well with the person and what kinda feelings it envokes, in context of one or multiple colors provided that will be provide by the request
-};
+    paletteInfo: string; // Explaining the general color palette, and why they were chosen for the user and the color psychology behind it
+    
+    jewelery: {
+      color: string ; // only gold or silver
+      message: // a message why this color works well with the person and what kinda feelings it envokes, in context of one or multiple colors provided that will be provide by the request
+    };
 }
 
 export interface IPalletteColor {
