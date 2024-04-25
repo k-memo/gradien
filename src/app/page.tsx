3a0debfea5d188f3a7f99fa808a8bfbd9d99a9c3
Helin Güyen <hgueyen.mmt-b2022@fh-salzburg.ac.at>
2024-04-25 18:13:42 +0200

    delete wave

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,12 +21,7 @@ export default function Home() {
             <div className="g6"></div>
           </div>
         </div>
-        <div className="wave-div">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 640">
-  <path fill="#ffffff" fill-opacity="1" d="M0,192L48,208C96,224,192,256,288,272C384,288,480,288,576,266.7C672,245,768,203,864,165.3C960,128,1056,96,1152,74.7C1248,53,1344,43,1392,37.3L1440,32L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
-</svg>
 
-        </div>
         <div className="landing-text">
           <div className="heading">
             <h1>
