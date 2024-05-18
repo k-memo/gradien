be69b069fdea489a21666cac89771f9438a3f72b
Helin Güyen <hgueyen.mmt-b2022@fh-salzburg.ac.at>
2024-05-18 23:30:18 +0200

    changes for the landing page

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,5 +1,10 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Logo() {
-  return <Image src="logo.svg" width={30} height={30} alt="logo" />;
+  return (
+    <Link href="/">
+      <Image src="/logo.svg" width={30} height={30} alt="logo" />
+    </Link>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,60 +2,43 @@
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Logo from '../../components/logo';
-import Wave from '../../public/wave.png';
+import Wave from '../../public/wave.svg';
 import './style/application.scss';
 import Link from 'next/link';
 import Contact from '../../components/contact';
 import About from '../../components/about';
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 import SideNavXl from '../../components/side-nav-xl';
+import Greeting from '../../components/greeting';
 
 export default function Home() {
   return (
     <>
       <SideNavXl />
       <main className="landing-page">
+        {/*Parallax
         <Parallax pages={2} style={{ top: '0', left: '0' }}>
           <ParallaxLayer offset={0} speed={0.5}>
-            <section className="landing-page-greeting">
-              <div className="gradient-bg">
-                <div className="gradient-container">
-                  <div className="g1"></div>
-                  <div className="g2"></div>
-                  <div className="g3"></div>
-                  <div className="g4"></div>
-                  <div className="g6"></div>
-                </div>
-              </div>
-
-              <div className="landing-text">
-                <div className="heading">
-                  <h1>
-                    GR
-                    <Logo />
-                    DIEN
-                  </h1>
-                  <p className="sub">FIND YOUR COLOR PALETTE</p>
-                </div>
-                <div>
-                  <Link href="generate" className="btn-main btn">
-                    Get Started
-                    <FontAwesomeIcon
-                      icon={faArrowRight}
-                      className="arrow-right"
-                    />
-                  </Link>
-                </div>
-              </div>
-            </section>
+            <Greeting />
           </ParallaxLayer>
+
           <ParallaxLayer offset={0} speed={3.5}>
             <div className="wave">
               <img src={Wave.src} alt="wave" className="wave-img" />
+              <About />
             </div>
           </ParallaxLayer>
         </Parallax>
+        */}
+        <Greeting />
       </main>
+
+      <section id="about">
+        <About />
+      </section>
+      <section id="contact">
+        <Contact />
+      </section>
     </>
   );
 }
