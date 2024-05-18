f2d58938312b70d0d3373f13b002ee3bb3ab26ad
Helin Güyen <hgueyen.mmt-b2022@fh-salzburg.ac.at>
2024-05-19 00:42:13 +0200

    side nav

--- components/side-nav-xl.tsx
+++ components/side-nav-xl.tsx
@@ -2,11 +2,17 @@ import { useState } from 'react';
 import Link from 'next/link';
 import Logo from './logo';
 import Menu from '../public/menu.svg';
-
+import Close from '../public/close.svg';
+import CloseS from '../public/close-s.svg';
+import { FiUser } from 'react-icons/fi';
+import { SlPlus } from 'react-icons/sl';
+import { FiBookmark } from 'react-icons/fi';
+import { FaCirclePlus } from 'react-icons/fa6';
 export default function SideNavXl() {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => {
+    console.log('Toggling menu');
     setIsOpen(!isOpen);
   };
   return (
@@ -16,12 +22,18 @@ export default function SideNavXl() {
       </div>
       <div className="nav-desktop">
         <div className="menu" onClick={toggleMenu}>
-          <img src={Menu.src} />
+          {isOpen ? <img src={CloseS.src} /> : <img src={Menu.src} />}
         </div>
         <div className={`links ${isOpen ? 'open' : ''}`}>
-          <Link href={''}>Link1</Link>
-          <Link href={''}>Link1</Link>
-          <Link href={''}>Link1</Link>
+          <Link className="user" href={''}>
+            <FiUser />
+          </Link>
+          <Link className="plus" href={''}>
+            <SlPlus size={'40px'} />
+          </Link>
+          <Link className="bookmark" href={''}>
+            <FiBookmark />
+          </Link>
         </div>
       </div>
       <div className="copyright">
