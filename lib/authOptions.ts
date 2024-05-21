612b6614304e34ee0858f035ee6960bef6be2d34
Helin Güyen <hgueyen.mmt-b2022@fh-salzburg.ac.at>
2024-05-21 10:49:27 +0200

    next auth

---
+++ lib/authOptions.ts
@@ -0,0 +1,14 @@
+import { NextAuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: 'jwt',
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+};
---
+++ src/app/login/form.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import React from 'react';
+import Link from 'next/link';
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+
+const LoginForm = () => {
+  return (
+    <>
+      <h2>Login</h2>
+      <div>sjkdfsdf</div>
+    </>
+  );
+};
+
+export default LoginForm;
