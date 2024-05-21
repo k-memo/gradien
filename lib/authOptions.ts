1fb030f343803d655552c2b0d4bf1eb7bf6c5bc0
Xava R <fhs49049@fh-salzburg.ac.at>
2024-05-21 16:42:56 +0200

    create user

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -1,6 +1,6 @@
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-
+import prisma from '../lib/prisma';
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID!;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET!;
 
@@ -14,4 +14,26 @@ export const authOptions: NextAuthOptions = {
       clientSecret: GOOGLE_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ account, profile}) {
+        if (!profile?.email){
+            throw new Error('No profile')
+        }
+        await prisma.user.upsert({
+          where: {
+            email: profile.email,
+          },
+          create: {
+            email: profile.email,
+            name: profile.name,
+
+          },
+          update: {
+            name: profile.name,
+          },
+        })
+        return true
+    },
+},
+
 };
