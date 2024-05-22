35175e0b9f61330fc867ef0f2879d64f5ddc47aa
Helin Güyen <hgueyen.mmt-b2022@fh-salzburg.ac.at>
2024-05-22 12:49:09 +0200

    resposnive design

--- components/about.tsx
+++ components/about.tsx
@@ -14,24 +14,24 @@ export default function About() {
           <h2>WHY YOU NEED COLOR ANALYSIS</h2>
           <div className="bullet-points">
             <div className="points">
-              <div>
+              <div className="point">
                 <span className="emoji">🎨</span>
                 <h4>PERSONALIZED STYLE</h4>
                 <p>Discover your colors for effortless, flattering outfits.</p>
               </div>
-              <div>
+              <div className="point">
                 <span className="emoji">👔</span>
                 <h4>PROFESSIONAL IMAGE</h4>
                 <p>Project competence with a polished appearance.</p>
               </div>
             </div>
             <div className="points">
-              <div>
+              <div className="point">
                 <span className="emoji">💃</span>
                 <h4>CONFIDENCE BOOST</h4>
                 <p>Feel confident and make lasting impressions.</p>
               </div>
-              <div>
+              <div className="point">
                 <span className="emoji">👗</span>
                 <h4>VERSATILE COORDINATION</h4>
                 <p>Mix and match effortlessly for cohesive style.</p>
