d38791168e506d659b70e11bf253da7f7dc4b6e3
Memo K <fhs49041@fh-salzburg.ac.at>
2024-05-22 13:30:39 +0200

    rollback about and contact

--- components/about.tsx
+++ components/about.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Image from 'next/image';
-
 import Gradien from '../public/blurgradien.png';
 
 export default function About() {
@@ -8,30 +6,30 @@ export default function About() {
     <>
       <div className="about-text">
         <div className="blurgradien">
-          <Image src={Gradien.src} alt="gradien box" />
+          <img src={Gradien.src} alt="gradien box" />
         </div>
         <div className="about-content">
           <h2>WHY YOU NEED COLOR ANALYSIS</h2>
           <div className="bullet-points">
             <div className="points">
-              <div className="point">
+              <div>
                 <span className="emoji">🎨</span>
                 <h4>PERSONALIZED STYLE</h4>
                 <p>Discover your colors for effortless, flattering outfits.</p>
               </div>
-              <div className="point">
+              <div>
                 <span className="emoji">👔</span>
                 <h4>PROFESSIONAL IMAGE</h4>
                 <p>Project competence with a polished appearance.</p>
               </div>
             </div>
             <div className="points">
-              <div className="point">
+              <div>
                 <span className="emoji">💃</span>
                 <h4>CONFIDENCE BOOST</h4>
                 <p>Feel confident and make lasting impressions.</p>
               </div>
-              <div className="point">
+              <div>
                 <span className="emoji">👗</span>
                 <h4>VERSATILE COORDINATION</h4>
                 <p>Mix and match effortlessly for cohesive style.</p>
