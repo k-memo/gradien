d7f8b634faeab5cced46d24b512f92e7d52583d6
Memo K <fhs49041@fh-salzburg.ac.at>
2024-05-24 00:17:47 +0200

    secret try bugfix

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -4,9 +4,10 @@ import prisma from '../lib/prisma';
 
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID!;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET!;
+const SECRET = process.env.SECRET!;
 
 export const authOptions: NextAuthOptions = {
-  secret: process.env.SECRET,
+  secret: SECRET,
   session: {
     strategy: 'jwt',
   },
