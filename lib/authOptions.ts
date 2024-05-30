d1b1c89df731f53a1c7575d37eef11f9ff2616c4
Helin Güyen <hgueyen.mmt-b2022@fh-salzburg.ac.at>
2024-05-30 17:04:48 +0200

    login fix

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -1,21 +1,15 @@
 import NextAuth, { NextAuthOptions } from 'next-auth';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import GoogleProvider from 'next-auth/providers/google';
 import prisma from '../lib/prisma';
-import { db } from './db';
 
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID!;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET!;
 const SECRET = process.env.SECRET!;
 
 export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(db as any),
   secret: SECRET,
   session: {
     strategy: 'jwt',
-  },
-  pages: {
-    signIn: '/login',
   },
   providers: [
     GoogleProvider({
@@ -24,40 +18,6 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   callbacks: {
-    async session({ token, session }) {
-      if (token) {
-        //@ts-ignore
-        session.user.id = token.id;
-        //@ts-ignore
-        session.user.name = token.name;
-        //@ts-ignore
-        session.user.email = token.email;
-        //@ts-ignore
-        session.user.image = token.picture;
-      }
-
-      return session;
-    },
-    async jwt({ token, user }) {
-      const dbUser = await db.user.findFirst({
-        where: {
-          email: token.email as string,
-        },
-      });
-
-      if (!dbUser) {
-        if (user) {
-          token.id = user?.id;
-        }
-        return token;
-      }
-
-      return {
-        id: dbUser.id,
-        name: dbUser.name,
-        email: dbUser.email,
-      };
-    },
     async signIn({ account, profile }) {
       if (!profile?.email) {
         throw new Error('No profile email');
